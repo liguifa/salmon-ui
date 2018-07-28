f3caf5bbae3f7c205ff8b37f8f6d3ee97db889cc
duch <543553257@qq.com>
2018-07-28 15:00:54 +0800

    添加input组件

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import SuiButton from "./components/button.component.vue"
 import SuiTabs from "./components/tabs.component.vue"
+import SuiInput from "./components/input.component.vue"
 
 const components = {
 	SuiButton,
-	SuiTabs
+	SuiTabs,
+	SuiInput,
 }
 
 if(window.Vue){
